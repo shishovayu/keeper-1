be377fd6fc5ffa10a9239e4e16762b2cc9c49ea6
nina <ninawinlee@gmail.com>
2018-12-18 15:38:19 +0300

    First commit

---
+++ Client/src/keeper/client/KeeperClientApplication.java
@@ -0,0 +1,17 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package keeper.client;
+
+import java.io.IOException;
+
+public class KeeperClientApplication {
+    public KeeperClientApplication() {
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Authorization("r_keeper");
+    }
+}
